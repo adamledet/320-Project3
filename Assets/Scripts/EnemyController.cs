710ad0e627973e55c9fe0d6decf06e7cc56f7ce4
trimute2 <ben.j.stern@gmail.com>
2018-11-15 21:32:12 -0500

    added basic enemy controller script, most everything about it is temporary

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class EnemyController : MonoBehaviour {
+
+	private CharacterController characterController;
+	// once we have more code for the player we might change this
+	private GameObject target;
+
+	public float maxSpeed;
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+
+	void OnEnable()
+	{
+		// this is temporary, im pretty sure calling find is expensive in terms of processing, will do this another way later
+		target = GameObject.Find("FPSController");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// this is also temporary, in future will make this more fleshed out
+		Vector3 toTarget = target.transform.position - this.transform.position;
+		Vector3 desiredVelocity = toTarget.normalized * maxSpeed;
+		if (!characterController.isGrounded)
+		{
+			desiredVelocity += Physics.gravity;
+		}
+		characterController.Move(desiredVelocity *Time.deltaTime);
+	}
+}
