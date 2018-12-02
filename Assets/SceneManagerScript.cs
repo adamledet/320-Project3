b1c06f9f31d9d7071068bf3c5cc714cb9caa21c0
Adam Ledet <acl6865@g.rit.edu>
2018-12-02 16:31:11 -0500

    Mian menu set up that will change scenes to Main Game.

---
+++ Assets/SceneManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Change Scene
+    public void ChangeScene(string sceneName)
+    {
+        Debug.Log("Loading");
+        SceneManager.LoadScene(sceneName);
+    }
+}
