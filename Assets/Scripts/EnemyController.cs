8ce34925b9496af31cdf401872a01de694436217
bjs2904 <bjs2904@IGM2000-09>
2018-12-14 19:23:56 -0500

    animation handeling faster deeaths rotation etc...

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,6 +12,15 @@ public class EnemyController : MonoBehaviour {
 	// once we have more code for the player we might change this
 	private GameObject target;
     private Animator animator;
+    private bool dying;
+
+    public bool Dying
+    {
+        get
+        {
+            return dying;
+        }
+    }
 	//the enemies health
 	private int health;
 
@@ -45,8 +54,9 @@ public class EnemyController : MonoBehaviour {
         animator = GetComponent<Animator>();
         navAgent = GetComponent<NavMeshAgent>();
 		navAgent.updatePosition = false;
-		navAgent.updateRotation = false;
+		navAgent.updateRotation = true;
 		navAgent.speed = maxSpeed;
+        dying = false;
 	}
 
 	void OnEnable()
@@ -57,29 +67,32 @@ public class EnemyController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// this is also temporary, in future will make this more fleshed out
-		//Move toward the target
-		if (KnockBack.magnitude > 0.2)
-		{
-			characterController.Move(KnockBack * Time.deltaTime);
-			KnockBack = Vector3.Lerp(KnockBack, Vector3.zero, 5*Time.deltaTime);
-		}
-		else
-		{
-			//Vector3 toTarget = target.transform.position - this.transform.position;
-			//Vector3 desiredVelocity = toTarget.normalized * maxSpeed;
-			navAgent.destination = target.transform.position;
-			Vector3 desiredVelocity = navAgent.desiredVelocity;
-			if (!characterController.isGrounded)
-			{
-				desiredVelocity += Physics.gravity;
-			}
-			characterController.Move(desiredVelocity * Time.deltaTime);
-		}
-		navAgent.velocity = characterController.velocity;
-        //Kill self if I fall off the edge of the map
-        if (transform.position.y < -30)
-            Die();
+        // this is also temporary, in future will make this more fleshed out
+        //Move toward the target
+        if (!dying)
+        {
+            if (KnockBack.magnitude > 0.2)
+            {
+                characterController.Move(KnockBack * Time.deltaTime);
+                KnockBack = Vector3.Lerp(KnockBack, Vector3.zero, 5 * Time.deltaTime);
+            }
+            else
+            {
+                //Vector3 toTarget = target.transform.position - this.transform.position;
+                //Vector3 desiredVelocity = toTarget.normalized * maxSpeed;
+                navAgent.destination = target.transform.position;
+                Vector3 desiredVelocity = navAgent.desiredVelocity;
+                if (!characterController.isGrounded)
+                {
+                    desiredVelocity += Physics.gravity;
+                }
+                characterController.Move(desiredVelocity * Time.deltaTime);
+            }
+            navAgent.velocity = characterController.velocity;
+            //Kill self if I fall off the edge of the map
+            if (transform.position.y < -30)
+                Die();
+        }
 	}
 
     //Take Damage
@@ -111,24 +124,28 @@ public class EnemyController : MonoBehaviour {
 		//Object.Destroy(gameObject);
         //manager.enemySpawns[Random.Range(0, 4)].GetComponent<SpawnEnemies>().spawnedEnemies -= 1;
         manager.RegisterDeath();
+        dying = true;
 	}
 
     //Collision /w/ Player
 	void OnControllerColliderHit(ControllerColliderHit col)
 	{
-		if (col.gameObject.tag == "Player")
-		{
-			col.gameObject.GetComponent<PlayerCollisions>().CollideWithEnemy(this);
-		}
-
-        if(col.gameObject.tag == "Fireball")
+        if (!dying)
         {
-            Damage(1);
-            col.gameObject.GetComponent<FireballMovement>().health -= 1;
+            if (col.gameObject.tag == "Player")
+            {
+                col.gameObject.GetComponent<PlayerCollisions>().CollideWithEnemy(this);
+            }
+
+            if (col.gameObject.tag == "Fireball")
+            {
+                Damage(1);
+                col.gameObject.GetComponent<FireballMovement>().health -= 1;
+            }
         }
 	}
 
-    public void OnDestroy()
+    public void Destroy()
     {
         Destroy(gameObject);
     }
